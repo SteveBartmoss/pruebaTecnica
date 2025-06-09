f1e2b272155f926368b2ecc1960f73113eda960d
Marco Antonio Guadalupe Rivas Guzman <marco.rivas@carsol.mx>
2025-06-09 17:10:01 -0600

    feat: Se completo el controlador

--- app/Http/Controllers/ProyectosController.php
+++ app/Http/Controllers/ProyectosController.php
@@ -4,22 +4,113 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Controllers\Controller;
+
+use Illuminate\Support\Facades\DB;
+use Exception;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+use App\Models\Proyectos;
+
+
 class ProyectosController extends Controller
 {
     
-    public function createProject(){
+    public function createProject(Request $request){
+
+        try{
+
+            Proyectos::updateOrCreate([
+                'id' => $request->id],[
+                    'name' => $request->name,
+                    'description' => $request->description,
+                    'status' => $request->status,
+                ]);
+
+            return response()->json([
+                'message' => 'Se creo el proyecto',
+            ],200);
+
+        }catch(\Throwable $th){
+
+            return response->json([
+                'error' => $th->getMessage(),
+                'trace' => $th->getTraceAsString(),
+                'message' => 'Error, consulta con soporte'
+            ],500);
+        }
+
 
     }
 
-    public function updateProject(){
+    public function updateProject(Request $request){
+
+        try{
+
+            $proyecto = Proyectos::findOrFail($request->id);
+
+            $proyecto->update([
+                'name' => $request->name,
+                'description' => $request->description,
+                'status' => $request->status,
+            ]);
+
+            return response()->json([
+                'message' => 'Se actualizo el proyecto',
+            ],200);
+
+        }catch(\Throwable $th){
+
+            return response->json([
+                'error' => $th->getMessage(),
+                'trace' => $th->getTraceAsString(),
+                'message' => 'Error, consulta con soporte'
+            ],500);
+        }
 
     }
 
-    public function getProject(){
+    public function getProject(Request $request){
+
+        try{
+
+            $projects = Proyectos::get();
+
+            return response()->json([
+                'info' => $projets,
+            ],200);
+
+        }catch(\Throwable $th){
+
+            return response->json([
+                'error' => $th->getMessage(),
+                'trace' => $th->getTraceAsString(),
+                'message' => 'Error, consulta con soporte'
+            ],500);
+        }
 
     }
 
-    public function deleteProject(){
+    public function deleteProject(Request $request){
         
+        try{
+
+            $proyecto = Proyectos::findOrFail($request->id);
+
+            $proyecto->delete();
+
+            return response()->json([
+                'message' => 'Proyecto eliminado',
+            ],200);
+
+        }catch(\Throwable $th){
+
+            return response->json([
+                'error' => $th->getMessage(),
+                'trace' => $th->getTraceAsString(),
+                'message' => 'Error, consulta con soporte'
+            ],500);
+        }
+
     }
 }
